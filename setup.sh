39738c989f50d1f50d4fdb894a15260ca1982c7c
shkumagai <take.this.2.your.grave@gmail.com>
2017-08-18 10:46:46 +0900

    Fix unexpected action

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 ARCH_NAME="PyQt5_gpl-5.8.2"
 
 curl -L https://downloads.sourceforge.net/project/pyqt/PyQt5/PyQt-5.8.2/${ARCH_NAME}.tar.gz | tar xvz
-cd PyQt5_gpl-5.8.2 \
+cd ${ARCH_NAME} \
     && python configure.py \
               --confirm-license \
               --disable=QtPositioning \
               --sip-incdir=/usr/local/include \
     && make && make install
-rm -rf ${ARCH_NAME}
+cd .. \
+    && rm -rf ${ARCH_NAME}
